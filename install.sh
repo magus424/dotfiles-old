64a2062a64ad5393d111195117d33e6fd2ea8778
Collin Grady <collin@collingrady.com>
2014-07-18 15:11:07 -0700

    added basic installer

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd $HOME
+ln -sf ~/.config/bashrc .bashrc
+ln -sf ~/.config/bash_profile .bash_profile
+ln -sf ~/.config/vim .vim
+ln -sf ~/.config/vim/vimrc .vimrc
+ln -sf ~/.config/vim/gvimrc .gvimrc
+ln -sf ~/.config/tmux.conf .tmux.conf
