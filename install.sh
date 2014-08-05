f0b9bf35ac78a200c44bd58c4f6149943cbb9dc9
Collin Grady <collin@collingrady.com>
2014-08-04 22:46:59 -0700

    added base git config

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 
 ln -sf ~/.config/bashrc ~/.bashrc
 ln -sf ~/.config/bash_profile ~/.bash_profile
+ln -sf ~/.config/gitconfig ~/.gitconfig
+ln -sf ~/.config/tmux.conf ~/.tmux.conf
 ln -sf ~/.config/vim ~/.vim
 ln -sf ~/.config/vim/vimrc ~/.vimrc
 ln -sf ~/.config/vim/gvimrc ~/.gvimrc
-ln -sf ~/.config/tmux.conf ~/.tmux.conf
 
 mkdir ~/.config/vim/bundle 2>/dev/null
 git clone https://github.com/gmarik/Vundle.vim.git ~/.config/vim/bundle/Vundle.vim
