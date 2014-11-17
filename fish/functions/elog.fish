43d2435dd479a1a7afb705175b385c4f13d11384
Collin Grady <collin@collingrady.com>
2014-11-17 14:25:14 -0800

    removed bestfriend plugin, added new twig snippets

--- fish/functions/elog.fish
+++ fish/functions/elog.fish
@@ -1,6 +1,6 @@
 function elog --description 'Tails lighttpd error log and greps for a user'
     if test -z $argv
-        tail -n 20 -f /var/log/lighttpd/sslerror.log | grep -A1 -B2 $USER
+        tail -n 20 -f /var/log/lighttpd/sslerror.log
     else
         tail -n 20 -f /var/log/lighttpd/sslerror.log | grep -A1 -B2 $argv
     end
