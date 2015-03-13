65afca5f3d11cb5d46a83e42458c52c1e66e976a
Collin Grady <collin@collingrady.com>
2015-03-12 18:22:20 -0700

    fixed elog to use -F, removed load marker from tmux, added php cache snippets, added/fixed some twig snippets, added use/namespace to php syntax

--- fish/functions/elog.fish
+++ fish/functions/elog.fish
@@ -1,7 +1,7 @@
 function elog --description 'Tails lighttpd error log and greps for a user'
     if test -z $argv
-        tail -n 20 -f /var/log/lighttpd/sslerror.log
+        tail -n 20 -F /var/log/lighttpd/sslerror.log
     else
-        tail -n 20 -f /var/log/lighttpd/sslerror.log | grep -A1 -B2 $argv
+        tail -n 20 -F /var/log/lighttpd/sslerror.log | grep -A1 -B2 $argv
     end
 end
